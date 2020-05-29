e302e3dcb9ddc3681e275766b6e0bab49e3b036f
Mark King <markpkng@gmail.com>
2020-05-29 18:55:16 -0400

    aggregate geo queries

--- __tests__/advanced.js
+++ __tests__/advanced.js
@@ -45,4 +45,74 @@ describe("advanced mongodb features", () => {
         expect(secondGroup.length).toBe(5);
         expect(secondGroup[0].name).toBe("Company 51");
     });
+
+    test("geoNear", async () => {
+        const SFCoordinates = [-122.4223791, 37.7679638];
+        const NYCoordinates = [-73.9758597, 40.7830649];
+        const ClientNearbyNYCoordinates = [-73.9795183, 40.784056];
+
+        let taxi = new Taxi();
+        taxi.brand = "Toyota";
+        taxi.model = "Yaris";
+        taxi.year = 2015;
+        taxi.owner = { name: "Driver 1", experience: 15 };
+        taxi.geometry = { coordinates: SFCoordinates };
+        taxi = await taxi.save();
+
+        let taxi2 = new Taxi();
+        taxi2.brand = "Toyota";
+        taxi2.model = "Yaris";
+        taxi2.year = 2015;
+        taxi2.owner = { name: "Driver 2", experience: 7 };
+        taxi2.geometry = { coordinates: NYCoordinates };
+        taxi2 = await taxi2.save();
+
+        const taxisNearbyOneKm = await Taxi.aggregate([
+            {
+                $geoNear: {
+                    near: {
+                        type: "Point",
+                        coordinates: ClientNearbyNYCoordinates,
+                    },
+                    spherical: true,
+                    maxDistance: 1 * 1000, //1 km
+                    distanceField: "dist.calculated",
+                },
+            },
+        ]);
+
+        expect(taxisNearbyOneKm[0].owner.name).toBe("Driver 2");
+
+        const taxisNearbyQuarterKm = await Taxi.aggregate([
+            {
+                $geoNear: {
+                    near: {
+                        type: "Point",
+                        coordinates: ClientNearbyNYCoordinates,
+                    },
+                    spherical: true,
+                    maxDistance: 0.25 * 1000, //1 km
+                    distanceField: "dist.calculated",
+                },
+            },
+        ]);
+
+        expect(taxisNearbyQuarterKm.length).toBe(0);
+
+        const taxisNearbyFiveThousandKm = await Taxi.aggregate([
+            {
+                $geoNear: {
+                    near: {
+                        type: "Point",
+                        coordinates: ClientNearbyNYCoordinates,
+                    },
+                    spherical: true,
+                    maxDistance: 5000 * 1000, //1 km
+                    distanceField: "dist.calculated",
+                },
+            },
+        ]);
+
+        expect(taxisNearbyFiveThousandKm.length).toBe(2);
+    });
 });
--- models/Taxi.js
+++ models/Taxi.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const PointSchema = new Schema({
+    type: { type: String, default: "Point" },
+    coordinates: { type: [Number], index: "2dsphere" }, //[lng, lat] mongodb uses longitude first
+});
+
 const OwnerSchema = new Schema({
     name: { type: String, required: true },
     experience: { type: Number, required: true },
@@ -21,6 +26,7 @@ const TaxiSchema = new Schema({
         },
     },
     owner: OwnerSchema,
+    geometry: PointSchema,
 });
 
 module.exports = mongoose.model("taxi", TaxiSchema);
