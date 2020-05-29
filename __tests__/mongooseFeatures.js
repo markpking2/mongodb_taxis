0f55e24c2117e5348b323ad9da9eb10470ba46a5
Mark King <markpkng@gmail.com>
2020-05-29 18:12:29 -0400

    mongo middleware tests

--- __tests__/mongooseFeatures.js
+++ __tests__/mongooseFeatures.js
@@ -61,4 +61,62 @@ describe("mongoose features", () => {
             );
         }
     });
+
+    test("post save middleware", async () => {
+        try {
+            let company = new Company();
+            company.name = "throw error name";
+            await company.save();
+        } catch (err) {
+            expect(err.message).toBe(`New Test Error`);
+        }
+    });
+
+    test("pre save middleware", async () => {
+        try {
+            let company = new Company();
+            company.name = "Mark's Company!!!";
+            await company.save();
+
+            const readCompany = await Company.findOne();
+            expect(readCompany.name).toBe("Marks Company");
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+    test("pre remove middleware", async () => {
+        try {
+            let company = new Company();
+            company.name = "First Company";
+            await company.save();
+
+            var taxi1 = new Taxi();
+            taxi1.brand = "Toyota";
+            taxi1.model = "Yaris";
+            taxi1.year = 2015;
+            taxi1.owner = { name: "Driver 1", experience: 15 };
+            taxi1 = await taxi1.save();
+
+            var taxi2 = new Taxi();
+            taxi2.brand = "Toyota";
+            taxi2.model = "Yaris";
+            taxi2.year = 2015;
+            taxi2.owner = { name: "Driver 2", experience: 7 };
+            taxi2 = await taxi2.save();
+
+            company.taxies = [taxi1.id, taxi2.id];
+            company = await company.save();
+
+            const taxiCount = await Taxi.countDocuments();
+            expect(taxiCount).toBe(2);
+
+            await company.delete();
+
+            const newTaxiCount = await Taxi.countDocuments();
+            expect(newTaxiCount).toBe(0);
+        } catch (err) {
+            console.log(err);
+        }
+    });
 });
